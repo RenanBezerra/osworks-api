1832fe4334900f9a11c0898f9c062b75cd3a3fa4
Renan Bezerra Gomes <renanbg90@gmail.com>
2020-04-24 20:46:16 -0300

    implementando buscar
    acrescentei javax persistense

--- src/main/java/com/gft/osworks/api/controller/ClienteController.java
+++ src/main/java/com/gft/osworks/api/controller/ClienteController.java
@@ -1,7 +1,9 @@
 package com.gft.osworks.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,20 +13,11 @@ import com.gft.osworks.domain.model.Cliente;
 @RestController
 public class ClienteController {
 
+	@PersistenceContext
+	private EntityManager manager;
+	
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		Cliente cliente1 = new Cliente();
-		cliente1.setId(1l);
-		cliente1.setNome("João");
-		cliente1.setTelefone("34 999-000");
-		cliente1.setEmail("joao@gmail.com");
-
-		Cliente cliente2 = new Cliente();
-		cliente2.setId(2l);
-		cliente2.setNome("Maria");
-		cliente2.setTelefone("34 11111-000");
-		cliente2.setEmail("maria@gmail.com");
-
-		return Arrays.asList(cliente1, cliente2);
+		return manager.createQuery("from Cliente", Cliente.class).getResultList();
 	}
 }
