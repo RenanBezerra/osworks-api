9f6b94a4ef67acc47f2b7f6f33e96d372a426677
Renan Bezerra Gomes <renanbg90@gmail.com>
2020-04-25 00:05:25 -0300

    alterando a mensagem para o idioma desejado
    alterei a mensagem de validação com o arquivo message.properties e alterei preferencies iso par UTF-8

--- src/main/java/com/gft/osworks/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/gft/osworks/api/exceptionhandler/ApiExceptionHandler.java
@@ -3,6 +3,9 @@ package com.gft.osworks.api.exceptionhandler;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +21,9 @@ import com.gft.osworks.api.exceptionhandler.Problema.Campo;
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
+	@Autowired
+	private MessageSource messageSource;
+	
 	@Override
 	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
 			HttpHeaders headers, HttpStatus status, WebRequest request) {
@@ -25,8 +31,8 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 		ArrayList<Campo> campos = new ArrayList<Problema.Campo>();
 		
 		for (ObjectError error : ex.getBindingResult().getAllErrors()) {
-			String nome = ((FieldError)error).getField(); 
-			String mensagem = error.getDefaultMessage();
+			String nome = ((FieldError)error).getField(); //para obter o valor do nome do objeto
+			String mensagem = messageSource.getMessage(error, LocaleContextHolder.getLocale());
 			
 			
 			campos.add(new Problema.Campo(nome, mensagem));
